582f7fb7c1b7be1a149d0c422e072eca939db57a
Maiju19 <maiju.hakulinen@myy.haaga-helia.fi>
2024-12-11 09:54:45 +0200

    Lisätty React+Vite tiedostot

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import LoginForm from './LoginForm'
+import './App.css' 
+function App (){
+
+  return (
+    <>
+      <div>
+      <h1>Tervetuloa sivustolleni!</h1>
+      <p>
+          <LoginForm />
+      </p>
+      </div>
+    </>
+  );
+
+}
+export default App
