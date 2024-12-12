64c85362146649056a7576c1e9eb25de2a4304ed
Maiju19 <maiju.hakulinen@myy.haaga-helia.fi>
2024-12-12 09:23:42 +0200

    Tehty tyylimuutokset

---
+++ firebaseConfig.ts
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDTzOXDHhpRdTGnHfPjHqkCK2P3EuhxcVg",
+    authDomain: "pilvi-valokuvaus.firebaseapp.com",
+    projectId: "pilvi-valokuvaus",
+    storageBucket: "pilvi-valokuvaus.firebasestorage.app",
+    messagingSenderId: "637070805840",
+    appId: "1:637070805840:web:bebc00bcd4a4fbed00ba55",
+    measurementId: "G-CF9VRJPCTC"
+  };
+
+  export default firebaseConfig;
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import LoginForm from './LoginForm'
+import './App.css' 
+function App (){
+
+  return (
+    <>
+      <div>
+      <h1>Tervetuloa sivustolleni!</h1>
+      
+          <LoginForm />
+      </div>
+    </>
+  );
+
+}
+export default App
