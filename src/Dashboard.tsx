e66952b8a4a3ee47fe23a0e19936a6a18bc14aab
Maiju19 <maiju.hakulinen@myy.haaga-helia.fi>
2024-12-16 08:24:57 +0200

    Otsikkoa muutettu

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -3,8 +3,12 @@ function Dashboard () {
   return (
     <div>
       <h2>Olet kirjautunut sisään!</h2>
+      <p>Täällä voit tutustua Kansallisgallerian taideteoksiin.</p>
     </div>
-    );
+
+  );
 }
 
 export default Dashboard;
+
+
